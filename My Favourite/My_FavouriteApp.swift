7b2a87eeef12cc1be7199e56835522834960103b
Kisari <seikakukisari@gmail.com>
2023-07-19 10:59:52 +0700

    Initial Commit

---
+++ My Favourite/My_FavouriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_FavouriteApp.swift
+//  My Favourite
+//
+//  Created by Minh Trương on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct My_FavouriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
