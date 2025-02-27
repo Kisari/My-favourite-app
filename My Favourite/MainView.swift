3349ab95d3660756703a18958e6c3d7d6a9d2f21
Minh Truong <minhtruong@AP-0821-1040.local>
2025-02-27 12:10:16 +0700

    clean MainView code

--- My Favourite/MainView.swift
+++ My Favourite/MainView.swift
@@ -8,82 +8,126 @@
 import SwiftUI
 
 struct MainView: View {
-  @State private var showInfo = false
+  @State private var showInfo: Bool = false
+  @State private var gradientOffsetAnimate: CGFloat = -200.0
   var body: some View {
     NavigationView {
       VStack(spacing: 0) {
-        ZStack {
-          Image("image-bg-1")
-            .resizable()
-            .edgesIgnoringSafeArea(.all)
-          VStack {
-            Image("logo-text-white")
-              .resizable()
-              .aspectRatio(contentMode: .fit)
-              .frame(width: 200, height: 200)
-              .padding(.leading, 150)
-              .padding(.bottom, 100)
-          }
-        }
-        .frame(width: Constant.Style.viewWidth, height: Constant.Style.viewHeight * 0.5)
+        MainViewBackground()
         ZStack {
           VStack {
-            VStack {
-              Text("My Favourite's")
-                .foregroundColor(.white)
-                .font(.custom("AbrilFatface-Regular", size: Constant.Spacing.xNormal))
-                .frame(maxWidth: .infinity, alignment: .leading)
-              Text("Food Places")
-                .foregroundColor(.white)
-                .font(.custom("AbrilFatface-Regular", size: Constant.Spacing.xNormal))
-                .frame(maxWidth: .infinity, alignment: .leading)
-              Text("Eat more, Know more")
-                .foregroundColor(.white)
-                .font(.custom("Zeyada", size: Constant.Spacing.large))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .multilineTextAlignment(.leading)
-            }
-            .padding(.leading, Constant.Spacing.xxLarge)
-            .padding([.bottom, .top], Constant.Spacing.xMedium)
-            
-            //Navigate to the main view
-            NavigationLink(destination: FoodListView().navigationBarBackButtonHidden(true)) {
-              Text("Start")
-                .foregroundColor(Color(.white))
-                .font(.custom("AbrilFatface-Regular", size: Constant.Spacing.normal))
-                .padding(.horizontal, Constant.Spacing.xLarge)
-                .padding(.vertical, Constant.Spacing.medium)
-            }
-            .cornerRadius(.infinity)
-            .navigationViewStyle(StackNavigationViewStyle())
-            .overlay(
-              RoundedRectangle(cornerRadius: Constant.Spacing.small)
-                .stroke(.white, lineWidth: 1))
-
-            HStack {
-              Spacer()
-              Button {
-                showInfo = true
-              } label: {
-                Image("info").resizable().frame(width: Constant.Spacing.xMedium, height: Constant.Spacing.xMedium)
-              }
-              .padding(.all, 5)
-              .alert(isPresented: $showInfo) {
-                Alert(title: Text("Best food places"),
-                      message: Text("Welcome and let enjoy the best food places with real experience all over VietNam cities"),
-                      dismissButton: Alert.Button.default(
-                        Text("Let's go"),
-                        action: {}
-                      ))
-              }
-              .cornerRadius(.infinity)
-            }
-            .padding([.trailing, .bottom], Constant.Spacing.large)
+            MainViewTile()
+            MainViewStartButton(gradientOffset: $gradientOffsetAnimate)
+            MainViewInstruction(showInfo: $showInfo)
           }
         }
         .frame(width: Constant.Style.viewWidth, height: Constant.Style.viewHeight * 0.5)
         .background(Color("palette-3"))
       }
+    }
+  }
+  
+  private struct MainViewBackground: View{
+    var body: some View{
+      ZStack {
+        Image("image-bg-1")
+          .resizable()
+          .edgesIgnoringSafeArea(.all)
+        VStack {
+          Image("logo-text-white")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 200, height: 200)
+            .padding(.leading, 150)
+            .padding(.bottom, 100)
+        }
+      }
+      .frame(width: Constant.Style.viewWidth, height: Constant.Style.viewHeight * 0.5)
+    }
+  }
+  
+  private struct MainViewTile: View{
+    var body: some View{
+      VStack (spacing: Constant.Spacing.xMedium){
+        VStack {
+          Text("My Favourite's")
+            .foregroundColor(.white)
+            .font(.custom("AbrilFatface-Regular", size: Constant.Spacing.xNormal))
+            .frame(maxWidth: Constant.Style.viewWidth, alignment: .leading)
+          Text("Food Places")
+            .foregroundColor(.white)
+            .font(.custom("AbrilFatface-Regular", size: Constant.Spacing.xNormal))
+            .frame(maxWidth: Constant.Style.viewWidth, alignment: .leading)
+        }
+        Text("Eat more, Know more")
+          .foregroundColor(.white)
+          .font(.custom("AbrilFatface-Regular", size: Constant.Spacing.normal))
+          .frame(maxWidth: Constant.Style.viewWidth, alignment: .leading)
+          .multilineTextAlignment(.leading)
+      }
+      .padding(.leading, Constant.Spacing.xxLarge)
+      .padding([.bottom, .top], Constant.Spacing.xMedium)
+    }
+  }
+  
+  private struct MainViewStartButton: View{
+    @Binding var gradientOffset: CGFloat
+    
+    var body: some View{
+      //Navigate to the food view list
+      NavigationLink(destination: FoodListView().navigationBarBackButtonHidden(true)) {
+        Text("Start")
+            .foregroundColor(Color(.white))
+            .font(.custom("AbrilFatface-Regular", size: Constant.Spacing.normal))
+            .padding(.horizontal, Constant.Spacing.xLarge)
+            .padding(.vertical, Constant.Spacing.medium)
+            .background(
+                RoundedRectangle(cornerRadius: Constant.Spacing.xSmall)
+                    .fill(
+                        LinearGradient(
+                            colors: [Color("palette-4"), Color("palette-3"), Color("palette-4")],
+                            startPoint: .leading,
+                            endPoint: .trailing)
+                    )
+                    .offset(x: gradientOffset)
+                    .blendMode(.screen)
+            )
+            .overlay(
+                RoundedRectangle(cornerRadius: Constant.Spacing.xSmall)
+                    .stroke(.white, lineWidth: 1)
+            )
+            .clipShape(RoundedRectangle(cornerRadius: Constant.Spacing.xSmall))
+            .onAppear {
+                withAnimation(.linear(duration: 1).repeatForever(autoreverses: false)) {
+                    gradientOffset = 200
+                }
+            }
+      }
+      .navigationViewStyle(StackNavigationViewStyle())
+    }
+  }
+  
+  private struct MainViewInstruction: View{
+    @Binding var showInfo: Bool
+    var body: some View{
+      HStack {
+        Spacer()
+        Button {
+          showInfo = true
+        } label: {
+          Image("info").resizable().frame(width: Constant.Spacing.xMedium, height: Constant.Spacing.xMedium)
+        }
+        .alert(isPresented: $showInfo) {
+          Alert(title: Text("Best food places"),
+                message: Text("Welcome and let enjoy the best food places with real experience all over VietNam cities"),
+                dismissButton: Alert.Button.default(
+                  Text("Let's go"),
+                  action: {}
+                ))
+        }
+        .cornerRadius(.infinity)
+      }
+      .padding([.trailing, .bottom], Constant.Spacing.large)
     }
   }
 }
--- My Favourite/View/Screen/FoodItemDetailView.swift
+++ My Favourite/View/Screen/FoodItemDetailView.swift
@@ -7,63 +7,7 @@
 
 import SwiftUI
 
-fileprivate struct FoodItemTitleSection: View{
-  @Binding var isClicked: Bool
-  var item: FoodPlace
-  var body: some View {
-    VStack(){
-      CustomImage(image: item.imageName, shape: "Rectangle")
-      HStack() {
-        VStack (alignment: .leading){
-          Text(item.name)
-            .bold()
-            .font(.system(size: Constant.Spacing.large))
-          
-          Text(item.address)
-            .fontWeight(.light)
-            .font(.subheadline)
-        }
-        Spacer()
-        HStack {
-          Button(action: {}, label: {
-            Image(systemName: "arrowshape.turn.up.forward.fill")
-          })
-          .font(.system(size: Constant.Spacing.normal))
-          Button(action: {
-            isClicked = !isClicked
-          }, label: {
-            isClicked ?
-              Image(systemName: "heart.fill")
-              :
-              Image(systemName: "heart")
-          })
-          .font(.system(size: Constant.Spacing.normal))
-          .foregroundColor(.red)
-        }
-      }
-      VStack(alignment: .leading) {
-        BadgeList(position: .trailing, tags: item.getFoodTags())
-      }
-    }
-    .padding(.horizontal, Constant.Spacing.small)
-  }
-}
-
-fileprivate struct FoodItemDetailSection: View{
-  var item: FoodPlace
-  var body: some View{
-    VStack(alignment: .leading) {
-      Text("Details")
-        .bold()
-        .font(.system(size: Constant.Spacing.large))
-      Text(item.content)
-        .font(.system(size: Constant.Spacing.normal))
-        .fontWeight(.regular)
-    }
-    .padding(.horizontal, Constant.Spacing.xMedium)
-  }
-}
-
+// fileprivate same as private (at file level)
 struct FoodItemDetailView: View {
   @State var toggleLike: Bool = true
   var item: FoodPlace
@@ -76,6 +20,63 @@ struct FoodItemDetailView: View {
     }
     .frame(maxWidth: Constant.Style.viewWidth, maxHeight: Constant.Style.viewHeight, alignment: .top)
     .cornerRadius(Constant.Spacing.xMedium)
+  }
+  
+  private struct FoodItemTitleSection: View {
+    @Binding var isClicked: Bool
+    var item: FoodPlace
+    var body: some View {
+      VStack {
+        CustomImage(image: item.imageName, shape: "Rectangle")
+        HStack {
+          VStack(alignment: .leading) {
+            Text(item.name)
+              .bold()
+              .font(.system(size: Constant.Spacing.xNormal))
+
+            Text(item.address)
+              .fontWeight(.light)
+              .font(.subheadline)
+          }
+          Spacer()
+          HStack {
+            Button(action: {}, label: {
+              Image(systemName: "arrowshape.turn.up.forward.fill")
+            })
+            .font(.system(size: Constant.Spacing.normal))
+            Button(action: {
+              isClicked = !isClicked
+            }, label: {
+              isClicked ?
+                Image(systemName: "heart.fill")
+                :
+                Image(systemName: "heart")
+            })
+            .font(.system(size: Constant.Spacing.normal))
+            .foregroundColor(.red)
+          }
+        }
+        VStack(alignment: .leading) {
+          BadgeList(position: .trailing, tags: item.getFoodTags())
+        }
+      }
+      .padding(.horizontal, Constant.Spacing.small)
+    }
+  }
+
+  private struct FoodItemDetailSection: View {
+    var item: FoodPlace
+    var body: some View {
+      VStack(alignment: .leading) {
+        Text("Details")
+          .bold()
+          .font(.system(size: Constant.Spacing.xNormal))
+        Text(item.content)
+          .font(.system(size: Constant.Spacing.normal))
+          .fontWeight(.regular)
+      }
+      .padding(.horizontal, Constant.Spacing.xMedium)
+    }
   }
 }
 
